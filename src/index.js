ea899d14bd343f3abb12f07662fd3abc2305a903
DONROB3R7 <costache.robert.valentin@gmail.com>
2020-11-18 18:00:06 +0100

    Intial Commit First Commit.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Layout extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Rex</h1>
+        <h1>Login Authentication</h1>
       </React.Fragment>
     );
   }
